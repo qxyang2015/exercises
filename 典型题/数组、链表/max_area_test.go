d2395f9db1b7052848d6fedb298e0689f56bb7bd
qxyang <819705349@qq.com>
2021-03-21 19:45:51 +0800

    开发相关代码

---
+++ 典型题/数组、链表/max_area_test.go
@@ -0,0 +1,12 @@
+package leetCode
+
+import (
+	"testing"
+)
+
+func TestMaxArea(t *testing.T) {
+	mArea := MaxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
+	if mArea != 49 {
+		t.Errorf("MaxArea([]int{1,8,6,2,5,4,8,3,7}) = %d; want 49", mArea)
+	}
+}
