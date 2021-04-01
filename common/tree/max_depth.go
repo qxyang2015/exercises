ce603aa308b1082a7767e52c4c496b8a29eae2a7
qxyang <819705349@qq.com>
2021-04-01 21:58:23 +0800

    树相关题目

---
+++ common/tree/max_depth.go
@@ -0,0 +1,24 @@
+package tree
+
+/*
+104. 二叉树的最大深度
+https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+*/
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return IntMax(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func IntMax(v1, v2 int) int {
+	if v1 > v2 {
+		return v1
+	}
+	return v2
+}
