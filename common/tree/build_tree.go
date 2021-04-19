cd64c0bd35e6ddab53218caabd834a3f8ff03b18
qxyang <819705349@qq.com>
2021-04-19 22:45:50 +0800

    构建二叉树

--- common/tree/build_tree.go
+++ common/tree/build_tree.go
@@ -5,8 +5,7 @@ package tree
 https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
 根据一棵树的前序遍历与中序遍历构造二叉树。
-注意:
-你可以假设树中没有重复的元素。
+注意:你可以假设树中没有重复的元素。
 */
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
