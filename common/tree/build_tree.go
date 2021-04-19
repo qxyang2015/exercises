e77d5cc41c7505fdb1b6ede3fb16f8528a069e65
qxyang <819705349@qq.com>
2021-04-19 22:44:28 +0800

    最小公共祖先、构建二叉树

---
+++ common/tree/build_tree.go
@@ -0,0 +1,26 @@
+package tree
+
+/*
+105. 从前序与中序遍历序列构造二叉树
+https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+根据一棵树的前序遍历与中序遍历构造二叉树。
+注意:
+你可以假设树中没有重复的元素。
+*/
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{preorder[0], nil, nil}
+	idx := 0
+	for ; idx < len(inorder); idx++ {
+		if preorder[0] == inorder[idx] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:idx+1], inorder[:idx])
+	root.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
+	return root
+}
