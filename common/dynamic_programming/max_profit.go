293b8ba9dc6b5f21a2d4839dd002d8714603262f
qxyang <819705349@qq.com>
2021-05-10 22:52:25 +0800

    股票买卖II

---
+++ common/dynamic_programming/max_profit.go
@@ -0,0 +1,24 @@
+package dynamic_programming
+
+/*
+122. 买卖股票的最佳时机 II
+https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+*/
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n < 2 {
+		return 0
+	}
+	dp := make([][2]int, n)
+	dp[0][1] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = Max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = Max(dp[i-1][1], dp[i-1][0]-prices[i])
+	}
+	return dp[n-1][0]
+}
